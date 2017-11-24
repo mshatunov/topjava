45da93f04e9a9758604b4d1a2765e0f17b845e56
Maxim Shatunov <mshatunov@alfabank.ru>
2017-11-24 12:10:29 +0300

    Make isBetween generic

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -1,23 +1,13 @@
 package ru.javawebinar.topjava.util;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-    public static boolean isBetween(LocalTime lt, LocalTime startTime, LocalTime endTime) {
-        return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0;
-    }
-
-    public static boolean isBetween(LocalDate date, LocalDate startDate, LocalDate finishDate) {
-        return date.compareTo(startDate) >= 0 && date.compareTo(finishDate) <= 0;
-    }
-
-    public static boolean isBetween(LocalDateTime dateTime, LocalDateTime startDateTime, LocalDateTime finishDateTime) {
-        return dateTime.compareTo(startDateTime) >= 0 && dateTime.compareTo(finishDateTime) <= 0;
+    public static <T extends Comparable<T>> boolean isBetween(T now, T start, T finish) {
+        return now.compareTo(start) >= 0 && now.compareTo(finish) <= 0;
     }
 
     public static String toString(LocalDateTime ldt) {
